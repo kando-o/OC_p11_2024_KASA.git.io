818f39ca2dee78839d566de1ad259cdf75d67112
Jason Kando <jkando@prismamedia.com>
2024-04-14 23:01:24 +0200

    P-11 Reformat root and router

--- src/home/views/home.jsx
+++ src/home/views/home.jsx
@@ -1,36 +1,44 @@
 import "../assets/styles/home.css"
 import "/src/components/footer/assets/styles/footer.css"
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Outlet, RouterProvider, useLocation } from 'react-router-dom'
 import Navigation from "../../components/navigation/views/navigation"
 import Footer from "../../components/footer/views/footer"
 import Galerie from "../../components/galerie/views/galerie"
 import Banner from "../../components/banner/views/banner"
+import Propos from "../../page/a_propos/views/a_propos"
 
 const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <>
-          <Navigation />
-          <Banner/>
-          <Galerie/>
-          <Footer/>
-        </>
-    },
-    {
-      path: '/a_propos',
-      element: <>
-        <Navigation /> 
-        <Footer/>
-      </>
-    },
-    {
-      path: '/404',
-      element: <>
-        <h1>Oups un petit manque de vigilance de nôtre part il s'emblerait</h1>
-        <Footer/>
-      </>
-    },
+  {
+    path: "/",
+    element: <Root/>,
+    children:[
+      {
+        path:"a_propos",
+        element: <Propos/>,
+      }
+    ]
+  }
 ])
+
+function Root () {
+  // Obtient le chemin actuel de l'URL
+  const {pathname} = useLocation()
+
+  const showGalerie = pathname !== '/a_propos' 
+  // Condition pour ne pas afficher Galerie sur la page /a_propos
+  const showBanner = pathname ==='/'
+
+  return <>
+    <Navigation/>
+      {showBanner && <Banner/>}
+      {/* Affiche Galerie seulement si le chemin n'est pas /a_propos */}
+      {showGalerie && <Galerie/>}
+      <div className="container">
+        <Outlet/>
+      </div>
+    <Footer/>
+  </>
+}
 
 function Home () {
     return <RouterProvider router={router}/>
