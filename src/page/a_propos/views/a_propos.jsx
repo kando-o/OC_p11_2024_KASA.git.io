226f56ab0bac6da5cd10ae93d1ec54b84dbf972a
Jason Kando <jkando@prismamedia.com>
2024-04-14 22:55:11 +0200

    P-11 add componant Propos

---
+++ src/page/a_propos/views/a_propos.jsx
@@ -0,0 +1,11 @@
+import "../styles/a_propos.css"
+
+function Propos () {
+    return <>
+    <div className="propos__banner">
+        <img src="../src/assets/paysage_montagne.jpg" alt="Paysage de montagne" />
+    </div>
+    </>
+}
+
+export default Propos
