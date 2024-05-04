7291a4e04c8422334332603afec32bc998fb4234
Jason Kando <jkando@prismamedia.com>
2024-05-04 03:16:22 +0200

    ADD page A PROPOS

--- src/page/a_propos/views/a_propos.jsx
+++ src/page/a_propos/views/a_propos.jsx
@@ -1,10 +1,25 @@
-import "../styles/a_propos.css"
+import "../assets/styles/a_propos.css"
+import Dropdown from "../../../components/dropdown/views/dropdown";
+import { useEffect, useState } from "react"
+
 
 function Propos () {
+
+    const [dataProp, setDataPropo] = useState(null)
+    useEffect(() => {
+        fetch("/src/data/about.json")
+        .then(response => response.json())
+        .then(jsonData => {
+            setDataPropo(jsonData)
+        })
+        .catch(error => console.error('Erreur lors de la récupération des données :', error))
+
+    }, [])
+    console.log("propos");
     return <>
-        <div className="propos__banner">
-            <img src="../public/assets/paysage_montagne.jpg" alt="Paysage de montagne" />
-        </div>
+        { dataProp &&
+            <Dropdown dataProp = {dataProp}></Dropdown >
+        }
     </>
 }
 
