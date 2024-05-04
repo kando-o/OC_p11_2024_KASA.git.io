64d06515c3688bb8dc963699f8209b2ebede8d4c
Jason Kando <jkando@prismamedia.com>
2024-05-04 03:14:05 +0200

    ADD componant BANNER / responsive à faire

--- src/components/banner/views/banner.jsx
+++ src/components/banner/views/banner.jsx
@@ -1,17 +1,38 @@
+import { useLocation } from 'react-router-dom'
+
 import '../assets/styles/banner.css'
 import '/public/assets/themes/fonts.css'
-// my-vue-app/src/assets/themes/fonts.css
 
 function Banner () {
 
+    const {pathname} = useLocation()
+
+    const BannerHome = pathname === "/"
+    const BannerPropos = pathname === "/a_propos"
+    const BannerFlat = pathname === "/flat"
+
     return <>
-        <figure className='banner'>
+        {BannerHome && 
+            <figure className='banner'>
                 <img src="/public/assets/imgBanner.png"  alt="paysage de montagne"  />
             <div className="banner__opacity"></div>
                 <figcaption>
                     <p className='banner__textBorder'>Chez vous, partout et ailleurs</p>
                 </figcaption>
-        </figure>
+        </figure> 
+        }
+
+        { BannerPropos && 
+            <div className="propos__banner">
+                <img src="../public/assets/paysage_montagne.jpg" alt="Paysage de montagne" />
+            </div>
+        }
+
+        { BannerFlat && 
+            <div className="flat__banner">
+                <img src="../public/assets/banner_flat.jpg" alt="Visuel de salon" />
+            </div>
+        }
     </>
 }
 
